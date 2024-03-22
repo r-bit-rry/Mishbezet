66c464364b81d603111b3d70eccd6626ef2ad0db
Roy Belio <robelio@microsoft.com>
2024-03-22 23:55:06 +0200

    change the table structure

--- src/WordList.js
+++ src/WordList.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import words from './wrds.json'; // Import the JSON file
+import words from "./wrds.json"; // Import the JSON file
 
 const WordListContainer = styled.div`
-  direction: rtl; // Add this line
-  overflow: auto; // Make it scrollable
-  height: 50vh; // Adjust this value to fit your needs
-  border: 1px solid #ccc; // Add a border to make it look like a frame
+    direction: rtl;
+    border: 1px solid #ccc;
 `;
 
 const Table = styled.table`
@@ -24,35 +22,46 @@ const Td = styled.td`
     padding: 10px;
 `;
 
+const TableBodyContainer = styled.div`
+    overflow: auto;
+    height: 50vh;
+`;
+
 function WordList({ selectedLetter }) {
     if (!words) {
         return <div>Select a letter to see the words</div>;
     }
 
-    const filteredWords = words.filter(word => word.hb.startsWith(selectedLetter));
+    const filteredWords = words.filter((word) =>
+        word.hb.startsWith(selectedLetter)
+    );
 
     return (
         <WordListContainer>
             <Table>
                 <thead>
                     <tr>
-                        <Th>מילה</Th>
-                        <Th>הגיה</Th>
-                        <Th>תרגום</Th>
-                        <Th>תדירות</Th>
+                        <Th style={{ width: "15%" }}>מילה</Th>
+                        <Th style={{ width: "20%" }}>הגיה</Th>
+                        <Th style={{ width: "55%" }}>תרגום</Th>
+                        <Th style={{ width: "10%" }}>תדירות</Th>
                     </tr>
                 </thead>
-                <tbody>
-                    {filteredWords.map((word, index) => (
-                        <tr key={index}>
-                            <Td>{word.hb}</Td>
-                            <Td>{word.phn.join(", ")}</Td>
-                            <Td>{word.eng.join(", ")}</Td>
-                            <Td>{index + 1}</Td>
-                        </tr>
-                    ))}
-                </tbody>
             </Table>
+            <TableBodyContainer>
+                <Table>
+                    <tbody>
+                        {filteredWords.map((word, index) => (
+                            <tr key={index}>
+                                <Td style={{ width: "15%" }}>{word.hb}</Td>
+                                <Td style={{ width: "20%" }}>{word.phn.join(", ")}</Td>
+                                <Td style={{ width: "55%" }}>{word.eng.join(", ")}</Td>
+                                <Td style={{ width: "10%" }}>{index + 1}</Td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </TableBodyContainer>
         </WordListContainer>
     );
 }
